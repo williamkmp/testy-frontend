9a0cbdf11d4e049bfeabbbd9207f5513d6509554
williamkmp <williamkurnia123@gmail.com>
2023-12-23 13:03:22 +0700

    feat: extract block left handle

--- pages/page/-component/block/paragraph.vue
+++ pages/page/-component/block/paragraph.vue
@@ -7,12 +7,24 @@ import Italic from '@tiptap/extension-italic';
 import Document from '@tiptap/extension-document';
 import Paragraph from '@tiptap/extension-paragraph';
 import { useEditorBodyStore } from '../../-store/editor-body';
+import BlockControl from './rignt-control.vue';
+import type { Block } from '~/types';
 
 const props = defineProps<{ index: number }>();
 const editorBody = useEditorBodyStore();
 const editor = computed({
-    get: () => editorBody.blockList.at(props.index)?.editor as Editor | undefined,
-    set: newValue => editorBody.blockList[props.index].editor = newValue,
+    get: () => {
+        const block = editorBody.blockList.at(props.index) as Block;
+        if (block.type === 'PARAGRAPH')
+            return block.editor as Editor;
+        else
+            return undefined;
+    },
+    set: (newValue) => {
+        const block = editorBody.blockList.at(props.index) as Block;
+        if (block.type === 'PARAGRAPH')
+            block.editor = newValue;
+    },
 });
 
 const isFocused = computed(() => editorBody.focusedBlock === props.index);
@@ -38,30 +50,34 @@ function handleDelete() {
 onBeforeMount(() => {
     // eslint-disable-next-line no-console
     console.log(`mounted - paragraph ${props.index}`);
-    const existingEditor = editorBody.blockList[props.index]?.editor as Editor | undefined;
-    editorBody.blockList[props.index].editor = new Editor({
-        content: existingEditor?.getJSON(),
-        extensions: [
-            Document,
-            Paragraph,
-            Text,
-            Underline,
-            Bold,
-            Italic,
-        ],
-        editorProps: {
-            attributes: { class: 'focus:outline-none w-full h-full' },
-        },
-        onBlur: () => editorBody.focusedBlock = -1,
-        onTransaction: (tr) => {
-            if (editor.value && editor.value.isFocused)
-                editorBody.focusedBlock = props.index;
-            if (tr.transaction.docChanged) {
-                // TODO: impelement transaction handling
-            }
-        },
-    });
-    (editorBody.blockList[props.index].editor as Editor).commands.focus('start');
+    editorBody.blockList[props.index].type = 'PARAGRAPH';
+    const block = editorBody.blockList[props.index];
+    if (block.type === 'PARAGRAPH') {
+        const existingEditor = block.editor as Editor;
+        block.editor = new Editor({
+            content: existingEditor?.getJSON(),
+            extensions: [
+                Document,
+                Paragraph,
+                Text,
+                Underline,
+                Bold,
+                Italic,
+            ],
+            editorProps: {
+                attributes: { class: 'focus:outline-none w-full h-full' },
+            },
+            onBlur: () => editorBody.focusedBlock = -1,
+            onTransaction: (tr) => {
+                if (editor.value && editor.value.isFocused)
+                    editorBody.focusedBlock = props.index;
+                if (tr.transaction.docChanged) {
+                    // TODO: impelement transaction handling
+                }
+            },
+        });
+        block.editor.commands.focus('start');
+    }
 });
 
 onUnmounted(() => {
@@ -74,46 +90,10 @@ onUnmounted(() => {
 <template>
     <div class="group flex items-start justify-start gap-1">
         <!-- NOTE: for vuedraggable to work there can only be one root node -->
-        <div
-            data-role="control"
-            class="flex items-center justify-center gap-0.5 transition duration-100 group-hover:opacity-100"
-            :class="[isFocused ? 'opacity-100' : 'opacity-0']"
-        >
-            <!-- menu & drag handle -->
-            <UPopover :popper="{ placement: 'left' }" @click="console.log('start')">
-                <template #default>
-                    <div class="rounded p-0.5 hover:bg-gray-200/50 dark:hover:bg-gray-100/10">
-                        <UIcon class="text-gray-500" name="i-heroicons-cog-6-tooth-solid" />
-                    </div>
-                </template>
-                <template #panel>
-                    <div class="relative p-1">
-                        <UButtonGroup orientation="vertical" size="xs">
-                            <UButton icon="i-heroicons-trash" label="Delete" color="gray" variant="ghost" />
-                            <UButton icon="i-heroicons-document-plus" label="Add below" color="gray" variant="ghost" />
-                            <UButton color="gray" variant="ghost" icon="i-material-symbols-format-h1-rounded" label="heading 1" />
-                            <UButton color="gray" variant="ghost" icon="i-material-symbols-format-h2-rounded" label="heading 2" />
-                            <UButton color="gray" variant="ghost" icon="i-material-symbols-format-h3-rounded" label="heading 3" />
-                            <UButton color="gray" variant="ghost" icon="i-material-symbols-text-fields-rounded" label="Text" />
-                            <UButton color="gray" variant="ghost" icon="i-material-symbols-format-list-bulleted-rounded" label="List" />
-                            <UButton color="gray" variant="ghost" icon="i-material-symbols-format-quote-rounded" label="Quote" />
-                            <UButton color="gray" variant="ghost" icon="i-material-symbols-rectangle-outline-rounded" label="Callout" />
-                            <UButton color="gray" variant="ghost" icon="i-material-symbols-folder" label="File" />
-                            <UButton color="gray" variant="ghost" icon="i-material-symbols-stacks-rounded" label="Collections" />
-                        </UButtonGroup>
-                    </div>
-                </template>
-            </UPopover>
-
-            <!-- drag handle -->
-            <div
-                :class="[editorBody.DRAGGABLE_CLASS]"
-                class="rounded px-1 py-0.5 hover:cursor-grab hover:bg-gray-200 dark:hover:bg-gray-100/10"
-            >
-                <UIcon class="text-gray-500" name="i-ic-round-drag-indicator" />
-            </div>
-        </div>
-
+        <BlockControl
+            :is-focused="isFocused"
+            @click-menu="editorBody.focusedBlock = props.index"
+        />
         <!-- block body -->
         <div class="w-full py-0.5">
             <BubbleMenu
---
+++ pages/page/-component/block/rignt-control.vue
@@ -0,0 +1,114 @@
+<script setup lang="ts">
+import { useEditorBodyStore } from '../../-store/editor-body';
+import type { BlockType } from '~/types';
+
+const props = defineProps<{ isFocused: boolean }>();
+const emit = defineEmits<{ change: [blockType: BlockType], delete: [], add: [], clickMenu: [] }>();
+
+const editorBody = useEditorBodyStore();
+const options = [
+    {
+        icon: 'i-heroicons-trash',
+        label: 'Delete',
+        click: () => emit('delete'),
+    },
+    {
+        icon: 'i-heroicons-plus-circle',
+        label: 'Add Below',
+        click: () => emit('add'),
+    },
+    {
+        icon: 'i-heroicons-trash',
+        label: 'Delete',
+        click: () => emit('delete'),
+    },
+    {
+        icon: 'i-material-symbols-format-h1-rounded',
+        label: 'Heading 1',
+        click: () => emit('change', 'HEADING_1'),
+    },
+    {
+        icon: 'i-material-symbols-format-h2-rounded',
+        label: 'Heading 2',
+        click: () => emit('change', 'HEADING_2'),
+    },
+    {
+        icon: 'i-material-symbols-format-h3-rounded',
+        label: 'Heading 3',
+        click: () => emit('change', 'HEADING_3'),
+    },
+    {
+        icon: 'i-material-symbols-text-fields-rounded',
+        label: 'Text',
+        click: () => emit('change', 'PARAGRAPH'),
+    },
+    {
+        icon: 'i-material-symbols-format-list-bulleted-rounded',
+        label: 'List',
+        click: () => emit('change', 'BULLET_LIST'),
+    },
+    {
+        icon: 'i-material-symbols-format-quote-rounded',
+        label: 'Quote',
+        click: () => emit('change', 'BLOCK_QUOTES'),
+    },
+    {
+        icon: 'i-material-symbols-rectangle-outline-rounded',
+        label: 'Callout',
+        click: () => emit('change', 'CALLOUT'),
+    },
+    {
+        icon: 'i-material-symbols-folder',
+        label: 'File',
+        click: () => emit('change', 'FILE'),
+    },
+    // TODO: implemnet collection option
+    // {
+    //     icon: 'i-material-symbols-stacks-rounded',
+    //     label: 'Collections',
+    //     click: () => emit('change', 'COLLECTION'),
+    // },
+];
+</script>
+
+<template>
+    <div
+        data-role="control"
+        class="flex items-center justify-center gap-0.5 transition duration-100 group-hover:opacity-100"
+        :class="[props.isFocused ? 'opacity-100' : 'opacity-0']"
+    >
+        <!-- menu  -->
+        <UPopover :popper="{ placement: 'left' }">
+            <template #default>
+                <div
+                    class="rounded p-0.5 hover:bg-gray-200/50 dark:hover:bg-gray-100/10"
+                    @click="$emit('clickMenu')"
+                >
+                    <UIcon class="text-gray-500" name="i-heroicons-cog-6-tooth-solid" />
+                </div>
+            </template>
+            <template #panel>
+                <div class="relative p-1">
+                    <UButtonGroup orientation="vertical" size="xs">
+                        <UButton
+                            v-for="menu in options"
+                            :key="menu.label"
+                            :label="menu.label"
+                            :icon="menu.icon"
+                            color="gray"
+                            variant="ghost"
+                        />
+                    </UButtonGroup>
+                </div>
+            </template>
+        </UPopover>
+
+        <!-- drag handle -->
+        <div
+            :class="[editorBody.DRAGGABLE_CLASS]"
+            class="rounded px-1 py-0.5 hover:cursor-grab hover:bg-gray-200 dark:hover:bg-gray-100/10"
+        >
+            <UIcon class="text-gray-500" name="i-ic-round-drag-indicator" />
+        </div>
+    </div>
+</template>
