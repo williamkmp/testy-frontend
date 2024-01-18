c5093217072881f835190aac127c1266452844b6
williamkmp <williamkurnia123@gmail.com>
2024-01-19 02:08:16 +0700

    feat: add Divider block component

---
+++ pages/page/-component/block/divider.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import BlockControl from './control/control.vue';
+import type { BlockEmit, BlockModel, BlockProps, BlockType } from '~/types';
+
+// Component Definition
+const props = defineProps<BlockProps>();
+const emit = defineEmits<BlockEmit>();
+const _block = defineModel<BlockModel>({ required: true });
+</script>
+
+<template>
+    <div class="group flex items-center justify-start gap-1">
+        <BlockControl
+            :is-focused="props.isFocused"
+            @click-menu="$emit('focus')"
+            @add="$emit('enter')"
+            @change="(blockType: BlockType) => emit('turn', blockType)"
+        />
+        <!-- block body -->
+        <div class="h-full w-full py-1">
+            <UDivider />
+        </div>
+    </div>
+</template>
