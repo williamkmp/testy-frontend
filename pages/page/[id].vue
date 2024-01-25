27a6219f4b4563c77d5384ffc3873a67942d02e0
williamkmp <williamkurnia123@gmail.com>
2024-01-25 19:27:59 +0700

    feat: extract page sekeleton loader to its own component

--- pages/page/[id].vue
+++ pages/page/[id].vue
@@ -2,6 +2,7 @@
 import EditorBody from './-component/editor-body.vue';
 import EditorBackgroundImage from './-component/editor-bacground-image.vue';
 import EditorHeader from './-component/editor-header.vue';
+import PageSkeletonLoader from './-component/page-skeleton-loader.vue';
 import { usePageDataStore } from './-store/page-data';
 import { useEditorBodyStore } from './-store/editor-body';
 import { createEditor, editorHTMLToJSON } from './-utils/editor-utils';
@@ -52,16 +53,7 @@ watchImmediate([() => pageData.iconKey, () => pageData.title], () => {
     <div :key="`page-${routeParam.id}`" class="flex h-full min-h-full w-full flex-col items-center">
         <!-- Page Loading View -->
         <template v-if="pending">
-            <header class="mb-4 flex w-full flex-col">
-                <USkeleton class="h-60 w-full rounded-none" />
-            </header>
-            <main class="mb-6 flex h-full w-full grow flex-col items-center">
-                <div class="flex h-full w-full max-w-4xl flex-col items-center gap-6 px-5">
-                    <USkeleton class="h-10 w-full rounded-md" />
-                    <USkeleton class="h-20 w-full rounded-md" />
-                    <USkeleton class="h-full w-full rounded-md" />
-                </div>
-            </main>
+            <PageSkeletonLoader />
         </template>
 
         <!-- Page Loaded View -->
