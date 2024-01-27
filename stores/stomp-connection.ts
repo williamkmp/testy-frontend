81f2186cc8f72f6ed1b06e6116f75929ffb59633
williamkmp <williamkurnia123@gmail.com>
2024-01-28 02:19:28 +0700

    feat: merge useStompClient and useStompConnectionStore

--- stores/stomp-connection.ts
+++ stores/stomp-connection.ts
@@ -1,28 +1,88 @@
+/* eslint-disable no-console */
 import { Client } from '@stomp/stompjs';
 
-export const useStompConnectionStore = defineStore('StompConnection', () => {
+export const useStompClient = defineStore('StompConnection', () => {
+    const app = useAppStore();
     const connection = ref<Client>();
 
-    async function connect() {
-        return new Promise((resolve) => {
+    async function getConnection() {
+        await new Promise((resolve) => {
             if (connection.value === undefined) {
                 connection.value = new Client({
                     brokerURL: useRuntimeConfig().public.WS_BASE_URL,
                     onConnect: () => resolve(true),
-                    onStompError: frame => console.error('[STOMP] err: ', frame),
+                    onStompError: frame => console.error('[STOMP] error: ', frame),
                 });
                 connection.value.activate();
             }
             else {
-                resolve(true);
+                resolve(false);
             }
         });
-    }
-
-    async function getConnection() {
-        await connect();
         return connection.value!;
     }
 
-    return { getConnection };
+    async function subscribe(
+        destiniation: string,
+        callback: (payload: any, header: Record<string, any>) => void,
+        retry?: boolean,
+    ) {
+        const connection = await getConnection();
+        try {
+            connection.subscribe(
+                destiniation,
+                payload => callback(JSON.parse(payload.body), payload.headers),
+                {
+                    sessionId: app.sessionId,
+                    userId: app.user!.id,
+                },
+            );
+        }
+        catch (error) {
+            console.error(`[STOMP]: Error subscribing to '${destiniation}'`);
+            console.error(error);
+            if (!retry)
+                await subscribe(destiniation, callback, true);
+        }
+    }
+
+    async function unsubscribe(destiniation: string) {
+        const connection = await getConnection();
+        try {
+            connection.unsubscribe(
+                destiniation,
+                {
+                    sessionId: app.sessionId,
+                    userId: app.user!.id,
+                },
+            );
+            console.log(`[STOMP]: Unsubscribed ${destiniation}`);
+        }
+        catch (error) {
+            console.error(`[STOMP]: Error unsubscribing to '${destiniation}'`);
+            console.error(error);
+        }
+    }
+
+    async function send(destiniation: string, payload: Record<string, any>) {
+        const connection = await getConnection();
+        try {
+            connection.publish({
+                destination: destiniation,
+                body: JSON.stringify(payload),
+                headers: {
+                    sessionId: app.sessionId,
+                    userId: app.user!.id,
+                },
+            });
+
+            console.log(`[STOMP]: Send message to ${destiniation}`);
+        }
+        catch (error) {
+            console.error(`[STOMP]: Error sending to '${destiniation}'`);
+            console.error(error);
+        }
+    }
+
+    return { subscribe, unsubscribe, send };
 });
