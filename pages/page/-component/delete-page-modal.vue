60b1f14e04dacd055ef7cdb6b1961973b4dc86db
williamkmp <williamkurnia123@gmail.com>
2024-02-07 00:52:36 +0700

    feat: implement delete basic functionality

--- pages/page/-component/delete-page-modal.vue
+++ pages/page/-component/delete-page-modal.vue
@@ -1,16 +1,40 @@
 <script setup lang="ts">
 import { useDeletePageModalStore } from '../-store/delete-page-modal';
+import { usePageDataStore } from '../-store/page-data';
 
+const path = useApiPath();
+const privateApi = usePrivateApi();
+const pageData = usePageDataStore();
 const deletePageModal = useDeletePageModalStore();
+const isLoading = ref(false);
+const notif = useNotification();
 
 async function doDeletePage() {
+    try {
+        isLoading.value = true;
+        if (pageData.id) {
+            const response = await privateApi.delete(path.pagePageId({ pageId: pageData.id }));
+            // TODO: implement page delete redirect
+        }
+        isLoading.value = false;
+        notif.ok({
+            title: 'Deleted',
+            message: 'Page successfully deleted',
+        });
+    }
+    catch (error) {
+        console.error(error);
+        notif.error({
+            title: 'Delete Error',
+            message: 'Please refresh the page.',
+        });
+    }
     deletePageModal.isOpen = false;
-    // TODO: implement delete page action
 }
 </script>
 
 <template>
-    <UModal v-model="deletePageModal.isOpen">
+    <UModal v-model="deletePageModal.isOpen" :prevent-close="isLoading">
         <UCard>
             <div class="mb-8">
                 <p class="w-full text-center text-lg font-semibold">
@@ -19,7 +43,7 @@ async function doDeletePage() {
             </div>
             <div class="flex items-center justify-center gap-6">
                 <div class="grow">
-                    <UButton label="Yes" color="red" variant="soft" block @click="doDeletePage" />
+                    <UButton label="Yes" color="red" variant="soft" block :loading="isLoading" @click="doDeletePage" />
                 </div>
             </div>
         </UCard>
